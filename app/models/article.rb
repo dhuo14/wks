a50757b92881f813b058aef859b3174962d346af
dhuo14 <dhuo14@163.com>
2014-04-25 11:09:14 +0800

    xml_form_helper

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,8 @@ class Article < ActiveRecord::Base
 	belongs_to :author, class_name: "User", foreign_key: "user_id"
 	has_one :content, class_name: "ArticleContent", :dependent => :destroy
 	# has_many :uploads, class_name: "ArcitleUpload"
-	has_and_belongs_to_many :article_categories
-	accepts_nested_attributes_for :article_categories, :content
+	has_and_belongs_to_many :categories, class_name: "ArticleCategory"
+	accepts_nested_attributes_for :categories, :content
  #  accepts_nested_attributes_for :uploads
 
 end
